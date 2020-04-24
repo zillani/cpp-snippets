09b95031f1f8f2a73d000b50a0c45c235e5d6be9
zillani shaik <shaikzillani@gmail.com>
2020-04-24 13:25:41 +0530

    initial commit
    Signed-off-by: zillani shaik <shaikzillani@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # cpp-snippets
+
+## Add executable
+[add executable](https://github.com/corochann/SingleFileExecutionPlugin)
---
+++ cmake-build-debug/CMakeFiles/listing_1.1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/listing_1.1.dir/concurrency/manning/listings/listing_1.1.cpp.obj"
+  "liblisting_1.1.dll.a"
+  "listing_1.1.exe"
+  "listing_1.1.exe.manifest"
+  "listing_1.1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/listing_1.1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
